7b5b2ea397b257e15bb6977b3f55e5970868ff7f
FernandaJuarez12 <LIZETHFERNANDAJUAREZGARCIA@Eaton.com>
2026-07-21 12:02:10 -0600

    EXAMPLE
    Removed Snowflake integration and smoothie customization features. Added a request to httpbin to check the status code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,8 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
+import requests
 
-session = get_active_session()
+st.title("🍓 Fernanda Smoothies")
 
-st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+response = requests.get("https://httpbin.org/get")
 
-st.write("""
-Choose the fruits you want in your custom Smoothie!
-""")
-
-name_on_order = st.text_input("Name on Smoothie")
-
-st.write("The name on your Smoothie will be", name_on_order)
-
-my_dataframe = session.table(
-    "smoothies.public.fruit_options"
-).select(col("FRUIT_NAME"))
-
-ingredients_list = st.multiselect(
-    "Choose up to 5 ingredients:",
-    my_dataframe,
-    max_selections=5
-)
-
-if ingredients_list:
-
-    ingredients_string = ""
-
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + " "
-
-    my_insert_stmt = """
-    INSERT INTO smoothies.public.orders
-    (ingredients, name_on_order)
-    VALUES
-    ('""" + ingredients_string + """','""" + name_on_order + """')
-    """
-
-    if st.button("Submit order"):
-        session.sql(my_insert_stmt).collect()
-
-        st.success(
-            f"Your Smoothie is ordered, {name_on_order}!",
-            icon="✅"
-        )
+st.write("Status Code:", response.status_code)
