8659f463782a92787d512669102bf86c54fc8530
FernandaJuarez12 <LIZETHFERNANDAJUAREZGARCIA@Eaton.com>
2026-07-21 12:23:21 -0600

    Remove unused import and session initialization

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
 
-session = get_active_session()
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 
