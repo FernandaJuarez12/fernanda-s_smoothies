67b8b56babc54cb103d19cc34d7828ba13e6f2e7
FernandaJuarez12 <LIZETHFERNANDAJUAREZGARCIA@Eaton.com>
2026-07-21 12:00:35 -0600

    Update streamlit_app.py
    Integrate Snowflake session and add smoothie ordering functionality.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,47 @@
 import streamlit as st
-import requests
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
-st.title("Fernanda Smoothies")
+session = get_active_session()
 
-response = requests.get("https://httpbin.org/get")
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 
-st.write(response.status_code)
+st.write("""
+Choose the fruits you want in your custom Smoothie!
+""")
+
+name_on_order = st.text_input("Name on Smoothie")
+
+st.write("The name on your Smoothie will be", name_on_order)
+
+my_dataframe = session.table(
+    "smoothies.public.fruit_options"
+).select(col("FRUIT_NAME"))
+
+ingredients_list = st.multiselect(
+    "Choose up to 5 ingredients:",
+    my_dataframe,
+    max_selections=5
+)
+
+if ingredients_list:
+
+    ingredients_string = ""
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + " "
+
+    my_insert_stmt = """
+    INSERT INTO smoothies.public.orders
+    (ingredients, name_on_order)
+    VALUES
+    ('""" + ingredients_string + """','""" + name_on_order + """')
+    """
+
+    if st.button("Submit order"):
+        session.sql(my_insert_stmt).collect()
+
+        st.success(
+            f"Your Smoothie is ordered, {name_on_order}!",
+            icon="✅"
+        )
