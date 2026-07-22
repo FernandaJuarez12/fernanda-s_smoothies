0865c5e6575c1f8b6a90c821b5bca7afe8f79d94
FernandaJuarez12 <LIZETHFERNANDAJUAREZGARCIA@Eaton.com>
2026-07-22 09:16:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,8 @@ ingredients_list = st.multiselect(
 if ingredients_list:
 
     for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information ')
 
         smoothiefroot_response = requests.get(
             "https://my.smoothiefroot.com/api/fruit/watermelon"
