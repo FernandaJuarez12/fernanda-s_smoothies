fc1fe7970e39c9eead0c1aeb59d91c5e01eae247
FernandaJuarez12 <LIZETHFERNANDAJUAREZGARCIA@Eaton.com>
2026-07-22 10:24:10 -0600

    Implement smoothie customization feature
    Added functionality for users to customize their smoothie by selecting ingredients and entering a name.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,25 @@
 import streamlit as st
 import requests
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+
+st.write("""
+Choose the fruits you want in your custom Smoothie!
+""")
+
+name_on_order = st.text_input(
+    "Name on Smoothie"
+)
+
+st.write(
+    "The name on your Smoothie will be",
+    name_on_order
+)
+
+ingredients_list = st.multiselect(
+    "Choose up to 5 ingredients:",
+    pd_df["FRUIT_NAME"].tolist(),
+    max_selections=5
+)
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 
