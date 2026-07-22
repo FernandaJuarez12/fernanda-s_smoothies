f07834a4e5314c17fae734c1084c0ba78c465879
FernandaJuarez12 <LIZETHFERNANDAJUAREZGARCIA@Eaton.com>
2026-07-22 09:23:12 -0600

    Change Orange to Lime in ingredients list

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.write("The name on your Smoothie will be", name_on_order)
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
-    ["Apple", "Banana", "Orange", "Mango", "Watermelon"],
+    ["Apple", "Banana", "Lime", "Mango", "Watermelon"],
     max_selections=5
 )
 
